bbfe7be9d3b7ebac3406113db8f347ced247fe05
uakh9 <22212484@bucks.ac.uk>
2023-10-16 13:38:24 +0100

    First commit PyGame

---
+++ TheGame/Player.py
@@ -0,0 +1,51 @@
+import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player = Player()
+
+running = True
+
+# Main loop
+while running:
+    # for loop through the event queue
+    for event in pygame.event.get():
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
+            # If the Esc key is pressed, then exit the main loop
+            if event.key == K_ESCAPE:
+                running = False
+        # Check for QUIT event. If QUIT, then set running to false.
+        elif event.type == QUIT:
+            running = False
+
+    # Fill the screen with black
+    screen.fill((0, 0, 0))
+
+    # Draw the player on the screen
+    screen.blit(player.surf, (SCREEN_WIDTH/2, SCREEN_HEIGHT/2))
+
+    # Update the display
+    pygame.display.flip()
---
+++ TheGame/Sprite.py
@@ -0,0 +1,51 @@
+import pygame
+import sys
+
+# Initialize Pygame
+pygame.init()
+
+# Constants
+WIDTH, HEIGHT = 700, 700
+BACKGROUND_COLOR = (255, 255, 255)
+SPRITE_COLOR = (0, 0, 0)
+SPRITE_SIZE = 50
+SPRITE_SPEED = 5
+
+# Create the game window
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Moving Sprite")
+
+# Initial sprite position
+sprite_x = (WIDTH - SPRITE_SIZE) // 2
+sprite_y = (HEIGHT - SPRITE_SIZE) // 2
+
+# Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Handle user input
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and sprite_x > 0:
+        sprite_x -= SPRITE_SPEED
+    if keys[pygame.K_RIGHT] and sprite_x < WIDTH - SPRITE_SIZE:
+        sprite_x += SPRITE_SPEED
+    if keys[pygame.K_UP] and sprite_y > 0:
+        sprite_y -= SPRITE_SPEED
+    if keys[pygame.K_DOWN] and sprite_y < HEIGHT - SPRITE_SIZE:
+        sprite_y += SPRITE_SPEED
+
+    # Clear the screen
+    window.fill(BACKGROUND_COLOR)
+
+    # Draw the sprite
+    pygame.draw.rect(window, SPRITE_COLOR, (sprite_x, sprite_y, SPRITE_SIZE, SPRITE_SIZE))
+
+    # Update the display
+    pygame.display.update()
+
+# Quit Pygame
+pygame.quit()
+sys.exit()
