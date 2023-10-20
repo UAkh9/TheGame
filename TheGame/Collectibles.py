039fc728ca43664d519660b023014f48a36f814b
AngeloSilva1 <naveensilva720@gmail.com>
2023-10-20 13:32:17 +0100

    made changes by Angelo

--- TheGame/Collectibles.py
+++ TheGame/Collectibles.py
@@ -1,4 +1,8 @@
 import pygame
+#Collect the collectibles if the player touches it
+Player_touched_collectible = Player_rectangle.colliderect(collectible)
+
+#Draw the collectibles 
 Collectibles_color = pygame.color(255, 0, 0)
 Collectibles_size_in_pixels = 20
 collectible_rectangle = pygame.rect (
