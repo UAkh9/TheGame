0343319d5ad33bdabac5c2c26b214944b8a87154
AngeloSilva1 <naveensila720@gmail.com>
2023-10-24 11:20:01 +0100

    Updates in the collectibles.py by ANGELO

--- TheGame/Collectibles.py
+++ TheGame/Collectibles.py
@@ -1,16 +1,26 @@
 import pygame
 
-SCREEN = pygame.display.set_mode ((800, 600))
+WIN = pygame.display.set_mode ((800, 600))
+WIDTH = 800
+HEIGHT = 600
 
-coinimage = pygame.image.load('coin.jpg')
-coinx = 370
-coiny = 480
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.quit:
             running = False
+
+pygame.quit()
+
+coin_image = pygame.image.load('Coin.jpg')
+coinx = 370
+coiny = 480          
+
+def draw_window():
+    WIN.blit(coin_image,(300,100))
+    pygame.display.update()
+
 
 
 
