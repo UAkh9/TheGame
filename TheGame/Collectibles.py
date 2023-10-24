5df1a3898782061e375c6c417eb5194f22c4b99d
AngeloSilva1 <naveensila720@gmail.com>
2023-10-24 11:19:30 +0100

    updates made by Angelo

--- TheGame/Collectibles.py
+++ TheGame/Collectibles.py
@@ -1,16 +1,18 @@
 import pygame
-#Collect the collectibles if the player touches it
-Player_touched_collectible = Player_rectangle.colliderect(collectible)
 
-#Draw the collectibles 
-Collectibles_color = pygame.color(255, 0, 0)
-Collectibles_size_in_pixels = 20
-collectible_rectangle = pygame.rect (
-    collectibles_x,
-    collectible_y,
-    collectible_size_in_pixels,
-    collectible_size_in_pixels)
-pygame.draw.rect (
-    screen,
-    Collectible_color,
-    collectible_rect)
+SCREEN = pygame.display.set_mode ((800, 600))
+
+coinimage = pygame.image.load('coin.jpg')
+coinx = 370
+coiny = 480
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.quit:
+            running = False
+
+
+
+
+
